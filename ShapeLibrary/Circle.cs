99db43067d711a76249e777064e27393228d0f36
romikkimor123@gmail.com <romikkimor123@gmail.com>
2024-04-26 12:14:40 +0300

    Добавить круг

---
+++ ShapeLibrary/Circle.cs
@@ -0,0 +1,18 @@
+﻿using ShapeLibrary.Interfaces;
+
+namespace ShapeLibrary;
+
+public class Circle : IFigureSquare
+{
+	private readonly double _radius;
+
+	public Circle(double radius)
+	{
+		_radius = radius;
+	}
+
+	public double GetSquare()
+	{
+		return Math.PI * Math.Pow(_radius, 2);
+	}
+}
